97a6c81d10762ec213e080c21a9bed1107f1ed96
Антон <antonolegovich2017@icloud.com>
2023-05-22 23:38:59 +0600

    dz

--- src/main/java/pro/sky/telegrambot/entity/Notification.java
+++ src/main/java/pro/sky/telegrambot/entity/Notification.java
@@ -1,12 +1,15 @@
 package pro.sky.telegrambot.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
 public class Notification {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long chatId;
